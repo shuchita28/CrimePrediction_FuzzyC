570d8e217883fab4d4d90a34a45f9dc6eea9bee9
Shuchita Mishra <71638384+shuchita28@users.noreply.github.com>
2021-03-06 05:36:28 -0500

    Add files via upload

---
+++ ConstantsData.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classificationcrime;
+
+/**
+ *
+ * @author Shuchita
+ */
+public class ConstantsData {
+  public  static String pathoffile="C:\\final year project\\13032018 code\\ClassificationCrime\\src\\";
+}
